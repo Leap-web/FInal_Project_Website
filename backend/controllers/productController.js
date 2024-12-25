b448a9b0eeecd201ab6ce0c2b332133d575a7594
Johnathann <bunleap.thay@student.cadt.edu.kh>
2024-12-25 16:07:16 +0700

    add backend syntax

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,12 @@
 const Product = require("../models/productModel");
-const upload = require('../config/multer');
+const upload = require("../config/multer");
 
 exports.getAllProducts = async (req,res) => {
     try{
         const products = await Product.getAll();
         title = "List product"
         res.render('product/index',{products,title});
-    
+        
     }catch(err){
         res.status(500).send("Error fetching products");
     }
@@ -26,11 +26,11 @@ exports.createProduct = async(req,res)=>{
             image_path = `/uploads/${req.file.filename}`;
         }
         await Product.create({ name, description, price, image: image_path });
-        // await Product.create(req.body);
         res.redirect("/product");
     }catch(err){
-        console.error(err.stack);
-        res.status(500).send("error creating product");
+        let backurl = '/product';
+        req.flash('error', err.sqlMessage);
+        return res.redirect(backurl);
     }
 }
 
@@ -65,15 +65,26 @@ exports.renderEditForm = async (req, res) => {
 // Update product
 exports.updateProduct = async (req, res) => {
     try {
-      await Product.update(req.params.id, req.body);
-      res.redirect('/product');
+        const { name, description, price } = req.body;
+        let image_path = "";
+
+        if (req.file) {
+            image_path = `/uploads/${req.file.filename}`;
+        }
+        else{
+            const product = await Product.getById(req.params.id);
+            image_path = product.image;
+            
+        }
+        await Product.update(req.params.id, { name, description, price, image: image_path });
+        
+        res.redirect('/product');
     } catch (err) {
-    console.log(err.stack);
       res.status(500).send('Error updating product');
     }
   };
   
-  // Delete product
+// Delete product
   exports.deleteProduct = async (req, res) => {
     try {
       await Product.delete(req.params.id);
--- mvc/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-// Middleware for overriding methods
-const methodOverride = require('method-override');
-router.use(methodOverride('_method'));
-
-// router.get('/login',userController.login);
